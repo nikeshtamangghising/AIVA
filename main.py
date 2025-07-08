74cb53c7c8edd24b8547cb9a3d25699c7682eaa3
Nikesh Tamang <nikeshtamangghising@gmail.com>
2025-07-08 23:39:57 +0545

    Downgrade python-telegram-bot to v13.7 for compatibility

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ import threading
 import random
 from datetime import datetime
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler
-from telegram.ext.filters import Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
 from telegram.error import Conflict, TelegramError, NetworkError
 import requests
 
